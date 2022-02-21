12d57dd3cbb51fb3c8c66ca6d502bd85cc40daab
JJbarton1 <1911755@rgu.ac.uk>
2022-02-21 18:23:26 +0000

    map

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-const characters = `https://api.punkapi.com/v2/`;
+const Drinks = `https://api.punkapi.com/v2/`;
+
+const maps = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters';
 
